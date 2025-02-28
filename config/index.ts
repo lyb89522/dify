d893fe8be0a050dbf710662ca10f7ea782152d7d
lyb89522 <66837793+lyb89522@users.noreply.github.com>
2025-02-28 21:49:08 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,16 +2,17 @@ import type { AppInfo } from '@/types/app'
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: 'Tech Chat For LESLEY',
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh-Hans'
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const isShowPrompt = true
+export const promptTemplate = ''
 
 export const API_PREFIX = '/api'
 
